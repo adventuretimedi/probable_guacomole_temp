3cfb7062309b26f79faaf7f1ede7e6e75928c319
Diana Peretiatko <olhadianaaa@gmail.com>
2023-03-07 18:58:56 +0200

    adding

--- js/main.js
+++ js/main.js
@@ -12,3 +12,36 @@ fetch('https://pokeapi.co/api/v2/pokemon/2')  //браузер, от на йди
         console.log(error);
 })
 //не якісний код вище, тому пишемо більш оптимальний
+
+const fetchUsersBtn = document.querySelector(".btn");
+const userList = document.querySelector(".user-list");
+
+fetchUsersBtn.addEventListener("click", () => {
+  fetchUsers()
+    .then((users) => renderUserList(users))
+    .catch((error) => console.log(error));
+});
+
+function fetchUsers() {
+  return fetch("https://jsonplaceholder.typicode.com/users").then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
+
+function renderUserList(users) {
+  const markup = users
+    .map((user) => {
+      return `<li>
+          <p><b>Name</b>: ${user.name}</p>
+          <p><b>Email</b>: ${user.email}</p>
+          <p><b>Company</b>: ${user.company.name}</p>
+        </li>`;
+    })
+    .join("");
+  userList.innerHTML = markup;
+}
